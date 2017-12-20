c0a56ba057d6b7851bd6f796eb7ac64fb8126c0f
Nicklason <Nicklason@users.noreply.github.com>
2017-12-20 20:31:29 +0100

    Added attributes to schema item

--- classes/CSchema.js
+++ classes/CSchema.js
@@ -49,7 +49,8 @@ CSchema.prototype.getItem = function(defindex) {
 				proper_name: item.proper_name || false,
 				item_class: item.item_class,
 				item_type_name: item.item_type_name,
-				image: item.image_url_large.replace('http://media.steampowered.com/apps/440/icons/', '')
+				image: item.image_url_large.replace('http://media.steampowered.com/apps/440/icons/', ''),
+				attributes: item.attributes || [];
 			};
 		}
 	}
